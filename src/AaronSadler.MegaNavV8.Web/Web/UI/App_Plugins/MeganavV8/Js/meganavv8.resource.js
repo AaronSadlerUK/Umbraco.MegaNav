77f6c59c1d0b35725f21d73ee9f95ece46079b1b
auroris <auroris@users.noreply.github.com>
2020-10-13 23:04:02 -0600

    Update meganavv8.resource.js

--- src/AaronSadler.MegaNavV8.Web/Web/UI/App_Plugins/MeganavV8/Js/meganavv8.resource.js
+++ src/AaronSadler.MegaNavV8.Web/Web/UI/App_Plugins/MeganavV8/Js/meganavv8.resource.js
@@ -2,7 +2,11 @@ angular.module("umbraco.resources").factory("meganavV8Resource", function ($http
     return {
         getById: function (id, culture) {
 
+<<<<<<< HEAD
             return $http.get("backoffice/MeganavV8/MeganavV8EntityApi/GetById?id=" + id + "&culture=" + (typeof culture === 'string' ? culture : culture.cultureName))
+=======
+            return $http.get("backoffice/MeganavV8/MeganavV8EntityApi/GetById?id=" + id + "&culture=" + culture)
+>>>>>>> parent of c5615d1... Update meganavv8.resource.js - expected string, got object
                 .then(function (response) {
                     var item = response.data;
                     item.icon = iconHelper.convertFromLegacyIcon(item.icon);
