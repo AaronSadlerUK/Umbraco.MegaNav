744bc18621fccd8052aa3b9838ec19a7bed973e8
Aaron Sadler <aaron@aaronsadler.co.uk>
2020-11-16 21:14:15 +0000

    Added configuration to disable umbraco cloud content sync

---
+++ src/AaronSadler.MegaNavV8.Core/AppSettingsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace AaronSadler.MegaNavV8.Core
+{
+    public static class AppSettingsManager
+    {
+        public static bool GetDisableUmbracoCloudSync()
+        {
+            return ConfigurationManager.AppSettings["DisableUmbracoCloudSync"] != null 
+                   && Convert.ToBoolean(ConfigurationManager.AppSettings["DisableUmbracoCloudSync"]);
+        }
+    }
+}
